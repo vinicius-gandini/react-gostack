ad2b0f0c3ec72d1ec684a2c251886fe75d9e0669
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-11-15 17:29:55 -0300

    feat: repository stylized :ok_hand:

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +12,43 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository :{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="" />
+          <div>
+            <strong />
+            <p />
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong />
+            <span />
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sdad">
+          <div>
+            <strong>asdsadsad</strong>
+            <p>sadd d asdsadsadsad</p>
+          </div>
+
+          <FiChevronRight size={24} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;

fe4bd7438d6204533a0f049ae8e99dcacedc5d6d
vinicius-gandini <viniciusgandini.goncalves@gmail.com>
2020-11-14 22:55:29 -0300

    feat: dashboard style :ok_hand:

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="" alt="Algo" />
+          <div>
+            <strong>sadsdaf</strong>
+            <p>fidjfd jfskdfhjkds fskdjfdskjf sdjkfksd fkd hksd</p>
+          </div>
+
+          <FiChevronRight size={24} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
